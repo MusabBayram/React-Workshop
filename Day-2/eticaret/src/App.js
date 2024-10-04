1b97561d3c4f59fca1cd6c1b0950c47665820e8a
Musab Bahadır Bayram <musab.bayram@hotmail.com>
2024-10-04 12:15:24 +0300

    eticaret klasörü yeniden eklendi

--- Day-2/eticaret/src/App.js
+++ Day-2/eticaret/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {Col, Container, Nav, Navbar, NavbarBrand, NavItem, NavLink, Row} from 'reactstrap';
+import {Link, Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import Cart from './components/Cart.js';
+import CategoryList from './components/CategoryList.js';
+import ProductList from './components/ProductList.js';
+import { categories, products } from './data/data.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () =>{
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (product) =>{
+    setCart([...cart, product]);
+  };
+
+  const handleRemoveFromCart = (product) => {
+    setCart(cart.filter(item => item.id !== product.id));
+  };
+
+  const handleClearCart = () =>{
+    setCart([]);
+  }
+
+  const filteredProducts = selectedCategory ? products.filter(product => product.categoryId === selectedCategory.id) : products;
+
+  return(
+    <Router>
+      <Container>
+          <Navbar color="light" expand="md">
+            <NavbarBrand tag={Link} to="/">E-Ticaret</NavbarBrand>
+            <Nav className='mr-auto'navbar>
+              <NavItem>
+                <NavLink tag={Link} to="/Cart">Sepet ({cart.length})</NavLink>
+              </NavItem>
+            </Nav>
+          </Navbar>
+          <Routes>
+            <Route
+            path='/'
+            element={
+              <Row>
+                <Col sm="4">
+                  <CategoryList categories={categories} onSelectedCategory={setSelectedCategory}></CategoryList>
+                </Col>
+                <Col sm="8">
+                  <ProductList products={filteredProducts} 
+                  onAddToCart={handleAddToCart}></ProductList>
+                </Col>
+              </Row>
+            }
+            />
+            <Route
+              path='/cart'
+              element={
+                <Cart cartItems={cart} onRemoveFromCart={handleRemoveFromCart} onClearCart={handleClearCart}></Cart>
+              }
+            >
+            </Route>
+          </Routes>
+      </Container>
+    </Router>
   );
-}
 
+}
 export default App;
